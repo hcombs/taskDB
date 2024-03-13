b1c4c6eef7b5e6ebd3b680862670a3b07f9f8aea
HC <hcc@HCC.myguest.virtualbox.org>
2024-03-13 14:16:58 -0400

    Added calendar controls for switching months and years

--- app/public/public/calendarControls.js
+++ app/public/public/calendarControls.js
@@ -75,23 +75,57 @@ const months = [
 
 var currentIndex = 0;
 var currentYear;
+var currentDay;
 
-const switchMonth = (i) => {
+const checkMonth = (i) => {
     currentIndex = currentIndex + i;
     currentIndex = currentIndex < 0 ? 11 : currentIndex > 11 ? 0 : currentIndex;
     return currentIndex;
 };
 
-const setYear = (year) => {
-    months.map(e=> e.year = year);
+const checkYear = (i) => {
+    let year = currentIndex + i < 0 ? currentYear + i  : currentIndex + i > 11 ? currentYear + i : currentYear;
+    if(currentYear !== year){
+        currentYear = year;
+        switchYear();
+    }
+    return i;
 };
+
+
+const setMonth = ()=>{
+    currentIndex = new Date().getMonth();
+};
+
+const setYear = (year) => {
+    months.map(e=> e.year = currentYear);
+};
+
+const setDay = () => {
+    currentDay = new Date().getDate();
+}
 
 const setLeapYearDay = () => {
     const isLeapYear = (months[1].year % 4 == 0 && months[1].year % 100 !== 0) || months[1].year % 400 == 0;
     months[1].days = isLeapYear ? 29 : 28;
 };
 
-const setStartDay = (month,year) => {
+const getStartDay = (month,year) => {
     return new Date(`${month}/1/${year}`).getDay();
 };
 
+const setStartDay = ()=> {
+    months.map((e,i)=>{
+        months[i].startIndex = getStartDay(i+1,months[i].year);
+    });
+};
+
+const getCurrentYear = () => {
+    currentYear = new Date().getFullYear();
+};
+
+const initializeMonths = composeAll(setLeapYearDay,setStartDay,setYear,getCurrentYear,setDay, setMonth)();
+
+const switchYear = composeAll(setLeapYearDay,setStartDay,setYear);
+
+const switchMonth = composeAll(checkMonth,checkYear);
